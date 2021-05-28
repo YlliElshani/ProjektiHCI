67bb8ad8f6acb724b3b66d33d341d7f2199c7c7c
Hysnijee <hysnijee.zllanoga@gmail.com>
2021-05-28 15:48:01 +0200

    initial commit

---
+++ logicComponents/header.php
@@ -0,0 +1,16 @@
+<div class="header">
+    <div class="left-header">
+        <ul>
+            <li><a href="#"><strong>E - Tickets</strong></a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+    <div class="right-header">
+        <ul>
+            <li><a href="#">LogIn</a></li>
+            <li><a href="#">SignUp</a></li>
+        </ul>
+    </div>
+</div>
