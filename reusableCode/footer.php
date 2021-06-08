a5b9e32a6a764b51fe389aaa8f48e07a86551d25
Hysnijee <hysnijee.zllanoga@gmail.com>
2021-06-08 23:06:40 +0200

    homepage and login done, login validation done

--- reusableCode/footer.php
+++ reusableCode/footer.php
@@ -1,4 +1,5 @@
 <div class="footer">
+    <hr>
     <ul class="footer-ul">
         <li>E - Tickets <br> Our Logo</li>
         <li>E - Tickets</li>
@@ -7,9 +8,39 @@
         <li>© E-Tickets, Inc. 2021. <br> We love our users!</li>
     </ul>
     <ul class="footer-ul">
-        <li><a href="">HI</a></li>
-        <li><a href="">HI</a></li>
-        <li><a href="">HI</a></li>
-        <li><a href="">HI</a></li>
+        <li><a href="https://www.facebook.com/"><img src="../media/facebook.png"></a></li>
+        <li><a href="https://twitter.com/home"><img src="../media/twitter.png" ></a></li>
+        <li><a href="https://www.instagram.com/"><img src="../media/instagram.png"></a></li>
+        <li><a href="https://www.reddit.com/"><img src="../media/reddit.png"></a></li>
     </ul>
 </div>
+<style>
+    .footer{
+        display: flex;
+        flex-direction: column;
+        padding:50px;
+    }
+    .footer hr{
+        width:85%;
+        align-self: center;
+        margin: 30px;
+    }
+    .footer-ul{
+        display: flex;
+        justify-content: center;
+        margin-top:50px;
+    }
+    .footer ul li a img{
+        width: 25px;
+        height:25px;
+    }
+    .footer-ul li {
+        margin-left: 100px;
+    }
+    ul li {
+    list-style-type: none;
+    }
+    ul li a{
+        text-decoration: none;
+    }
+</style>
