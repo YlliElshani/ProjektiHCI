ecf92c69b9162e80ca07a6021acfb9f519e86d1f
Hysnijee <hysnijee.zllanoga@gmail.com>
2021-06-12 20:49:56 +0200

    updated database

--- hci_db.sql
+++ hci_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2021 at 02:21 PM
+-- Generation Time: Jun 12, 2021 at 08:48 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -20,6 +20,18 @@ SET time_zone = "+00:00";
 --
 -- Database: `hci_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favourites`
+--
+
+CREATE TABLE `favourites` (
+  `favId` int(11) NOT NULL,
+  `movie` int(11) DEFAULT NULL,
+  `user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -44,6 +56,18 @@ INSERT INTO `movieinfo` (`ID`, `Emri`, `Cmimi`, `Pershkrimi`, `fotosource`, `fot
 (1, 'Wrath of Man', '3.99$', 'Adapted from the 2004 French film \"Le Convoyeur\" (aka \"Cash Truck\"), and borrowing the basic outline of the story, \"Wrath of Man\" is a time-shifting neo-noir crime thriller, filled with tough, sometim', '../media/detailsphoto1.jpg', '../media/detailsphoto2.jpg'),
 (2, 'Before we Go', '5.00$', 'Two strangers stuck in Manhattan for the night grow into each other\'s most trusted confidants when an evening of unexpected adventure forces them to confront their fears and take control of their live', '../media/detailsphoto3.jpg\r\n', '../media/detailsphoto4.jpg'),
 (3, 'Interstellar ', '2.10$', 'Interstellar is a 2014 British-American epic science fiction film co-written, directed and produced by Christopher Nolan. ... Set in a dystopian future where humanity is struggling to survive, the fil', '../media/detailsphoto5.jpg', '../media/detailsphoto6.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `ticketid` int(11) NOT NULL,
+  `movie` int(11) DEFAULT NULL,
+  `user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -74,10 +98,26 @@ INSERT INTO `user` (`userid`, `name`, `lastname`, `email`, `password`, `ccNo`, `
 --
 
 --
+-- Indexes for table `favourites`
+--
+ALTER TABLE `favourites`
+  ADD PRIMARY KEY (`favId`),
+  ADD KEY `movie` (`movie`),
+  ADD KEY `user` (`user`);
+
+--
 -- Indexes for table `movieinfo`
 --
 ALTER TABLE `movieinfo`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`ticketid`),
+  ADD KEY `movie` (`movie`),
+  ADD KEY `user` (`user`);
 
 --
 -- Indexes for table `user`
@@ -94,6 +134,24 @@ ALTER TABLE `user`
 --
 ALTER TABLE `user`
   MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `favourites`
+--
+ALTER TABLE `favourites`
+  ADD CONSTRAINT `favourites_ibfk_1` FOREIGN KEY (`movie`) REFERENCES `movieinfo` (`ID`),
+  ADD CONSTRAINT `favourites_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`userid`);
+
+--
+-- Constraints for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`movie`) REFERENCES `movieinfo` (`ID`),
+  ADD CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`userid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
